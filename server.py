aeee6814193a5129cafe218ee368d1c6261406b4
kranthikiran01 <guduru.kranthikiran@gmail.com>
2016-01-28 09:36:42 +0530

    Scrape articles

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 from flask import Flask,render_template
+import newspaper,json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/api/v1/scrape-articles/<site>')
+def apiScrapeArticles(site=None):
+	print site
+	paper=newspaper.build("http://"+site+".com")
+	for article in paper.articles:
+		print article.url
+	return "hello"
 
 if __name__ == "__main__":
     app.run()
