474bb0d8f2d744cfd4cf1e3ddd4f4ff2559425f6
kranthikiran01 <guduru.kranthikiran@gmail.com>
2016-01-29 13:09:32 +0530

    FIX

--- server.py
+++ server.py
@@ -1,29 +1,31 @@
 from flask import Flask,render_template
 from flask_restplus import Resource, Api
-import newspaper,json
+import newspaper,json,urllib
 app = Flask(__name__)
 api = Api(app)
 
 @api.route('/api/v1/get-articles/<string:site>')
 @api.doc(params={'site':"Site name without http prefix Ex:ndtv.com"})
-class Articles(Resource):
+class ArticlesList(Resource):
 	def get(self,site):
-		print site
-		paper=newspaper.build("http://"+site)
+		paper=newspaper.build("http://"+site,memoize_articles=False)
 		articles={}
 		i=0
 		for article in paper.articles:
 			articles[i]={}
 			articles[i]['url']=article.url
-			article.download()
-			article.parse()
-			article.nlp()
-			articles[i]['text']=article.text
-			articles[i]['authors']=article.authors
-			articles[i]['summary']=article.summary
-			articles[i]['keywords']=article.keywords
 			i=i+1
 		return {'articles':articles}
+
+resource_fields = {
+	'site_url':fields.Url,
+}
+@api.route('/api/v1/scrape-article/<string:site_url>')
+class ArticleInfo(Resource):
+	def get(self,site_url):
+		url=urllib.unquote(site_url)
+		print site_url
+		return {'message':"hello world"}
 
 @api.route('/index')
 class Home(Resource):
