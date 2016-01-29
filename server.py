8c4eda71fbd4b92b18657d307e28fcff160b682b
kranthikiran01 <guduru.kranthikiran@gmail.com>
2016-01-30 01:02:08 +0530

    Adding NLP to find person related data in article and name/profession check in scraping

--- server.py
+++ server.py
@@ -1,42 +1,65 @@
 from flask import Flask
-from flask_restplus import Resource, Api
-import newspaper,json,urllib
+from flask_restplus import Resource, Api, fields, marshal_with
+import newspaper,json
+from newspaper import Article
+import nltk
 app = Flask(__name__)
 api = Api(app)
 
 @api.route('/api/v1/get-articles/<string:site>')
 @api.doc(params={'site':"Site name without http prefix Ex:ndtv.com"})
-class ArticlesList(Resource):
+class ArticleList(Resource):
 	def get(self,site):
-		paper=newspaper.build("http://"+site,memoize_articles=False)
+		print site
+		paper=newspaper.build("http://"+site)
 		articles={}
 		i=0
 		for article in paper.articles:
 			articles[i]={}
 			articles[i]['url']=article.url
-<<<<<<< HEAD
-=======
-			# article.download()
-			# article.parse()
-			# article.nlp()
-			# articles[i]['text']=article.text
-			# articles[i]['authors']=article.authors
-			# articles[i]['summary']=article.summary
-			# articles[i]['keywords']=article.keywords
->>>>>>> 737b67b72255bfa03e52d3e3fbf3696aae283306
 			i=i+1
 		return {'size':i,'articles':articles}
 
-resource_fields = {
-	'site_url':fields.Url,
-}
-@api.route('/api/v1/scrape-article/<string:site_url>')
+@api.route('/api/v1/scrape-article/<path:url>/<string:name>/<string:profession>')
 class ArticleInfo(Resource):
-	def get(self,site_url):
-		url=urllib.unquote(site_url)
-		print site_url
-		return {'message':"hello world"}
-
+	def get(self,url,name,profession):
+		article = Article(url)
+		article.download()
+		article.parse()
+		article.nlp()
+		article_data = {}
+		article_data['url']=url
+		article_data['title']=article.title
+		article_data['keywords']=article.keywords
+		article_data['summary']=article.summary
+		article_data['text']=article.text
+		article_data['top_image']=article.top_image
+		article_data['publish_date']=str(article.publish_date)
+		article_data['authors']=article.authors
+		article_data['movies']=article.movies
+		article_data['html']=article.html
+		###################################
+		###### ToDo: NLP checks goes here
+		for sent in nltk.sent_tokenize(article.text):
+			for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+				if hasattr(chunk, 'node'):
+					if chunk.node=="PERSON":
+						print chunk.node, ' '.join(c[0] for c in chunk.leaves())
+		nameHit=False
+		profHit=False
+		firstNameHit=False
+		lastNameHit=False
+		nameList=name.split()
+		for x in article.text.split():
+			if x.lower()==nameList[0].lower():
+				firstNameHit=True
+			if x.lower()==nameList[1].lower():
+				lastNameHit=True
+			if x.lower()==profession.lower():
+				profHit=True
+		if firstNameHit==True and lastNameHit==True:
+			nameHit=True
+		return {'article':article_data,'name_hit':nameHit,'profession_hit':profHit}
 @api.route('/index')
 class Home(Resource):
     def get(self):
@@ -44,4 +67,4 @@ class Home(Resource):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
